fe84c6e4c0c4ab9fcecfaffb47d111ee607d6573
Syed <rh_syed@outlook.com>
2025-04-05 18:51:57 -0600

    Get User On Search

---
+++ app/search/index.tsx
@@ -0,0 +1,3 @@
+// app/search/index.tsx
+import SearchScreen from '@/src/screens/SearchScreen';
+export default SearchScreen;
---
+++ src/models/User.ts
@@ -0,0 +1,8 @@
+export interface User {
+    login: string;
+    name: string;
+    avatar_url: string;
+    bio: string;
+    followers: number;
+    following: number;
+}
